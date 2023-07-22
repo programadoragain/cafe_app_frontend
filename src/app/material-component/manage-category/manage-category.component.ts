b1f3c9186dacaa16fe4d4cef1b21faaa80c778bb
programadoragain <ferdandi04@gmail.com>
2023-07-22 13:01:36 -0300

    changes in manage-category

--- src/app/material-component/manage-category/manage-category.component.ts
+++ src/app/material-component/manage-category/manage-category.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryComponent } from '../dialog/category/category.component';
+import { CategoryService } from 'src/app/services/category.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { GlobalConstants } from 'src/app/shared/global-constants';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-manage-category',
@@ -8,23 +15,50 @@ import { CategoryComponent } from '../dialog/category/category.component';
 })
 export class ManageCategoryComponent implements OnInit {
 
-  constructor() {}
+  displayedColums: string[] = ["name", "edit"];
+  dataSource: any;
+  responseMessage: any;
+
+  constructor(private categoryService: CategoryService, private ngxService: NgxUiLoaderService, private dialog: MatDialog, private snackbarService: SnackbarService, private router: Router) { }
 
   ngOnInit(): void {
+    this.ngxService.start();
+    this.tableData();
   }
+
+  tableData() {
+    this.categoryService.getCategorys().subscribe((response: any) => {
+      this.ngxService.stop();
+      this.dataSource = new MatTableDataSource(response);
+    }, (error: any) => {
+      this.ngxService.stop();
+      console.log(error.error?.message);
+      if (error.error?.message) this.responseMessage = error.error?.message;
+      else
+        this.responseMessage = GlobalConstants.genericError;
+
+      this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 
   handleAddAction() {
     const dialogConfig = new MatDialogConfig();
-    dialogConfig.data = { 
+    dialogConfig.data = {
       action: 'Add'
     };
     dialogConfig.width = "850px";
-    const dialogRef= this.dialog.open(CategoryComponent ,dialogConfig);
+    const dialogRef = this.dialog.open(CategoryComponent, dialogConfig);
     this.router.events.subscribe(() => dialogRef.close());
 
-    const sub= dialogRef.componentInstance.onAddCategory.subscribe((response) => this.tableData());
+    const sub = dialogRef.componentInstance.onAddCategory.subscribe((response:any) => this.tableData());
   }
-  
+
   handleEditAction(values: any) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.data = {
@@ -32,9 +66,9 @@ export class ManageCategoryComponent implements OnInit {
       data: values
     };
     dialogConfig.width = "850px";
-    const dialogRef= this.dialog.open(CategoryComponent ,dialogConfig);
+    const dialogRef = this.dialog.open(CategoryComponent, dialogConfig);
     this.router.events.subscribe(() => dialogRef.close());
 
-    const sub= dialogRef.componentInstance.onAddCategory.subscribe((response) => this.tableData());
+    const sub = dialogRef.componentInstance.onAddCategory.subscribe((response:any) => this.tableData());
   }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+url= environment.apiUrl;
+constructor(private httpClient: HttpClient) {}
+
+add (data: any) {
+return this.httpClient.post(this.url + "category/add", data, 
+{ headers: new HttpHeaders().set('Content-Type', 'application/json') });
+}
+
+update(data: any) {
+return this.httpClient.post(this.url + "category/update", data, 
+{ headers: new HttpHeaders().set('Content-Type', 'application/json') });
+}
+
+getCategorys() {
+return this.httpClient.get(this.url + "category/get");
+}
+
 }
