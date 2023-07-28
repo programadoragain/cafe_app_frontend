c749fac37c744b85dfb3af4699c0a3b2974c5345
programadoragain <ferdandi04@gmail.com>
2023-07-27 21:24:12 -0300

    .

--- src/app/material-component/manage-order/manage-order.component.ts
+++ src/app/material-component/manage-order/manage-order.component.ts
@@ -53,8 +53,68 @@ export class ManageOrderComponent implements OnInit {
 
   getCategorys() {
     this.categoryService.getCategorys().subscribe((response:any) => {
+      this.ngxService.stop();
       this.categorys= response;
     });
   }
 
+  getProductByCategory(values: any) {
+    this.productService.getProductByCategory(values.id).subscribe((response:any) => {
+      this.products= response;
+      this.manageOrderForm.controls['price'].setValue('');
+      this.manageOrderForm.controls['quantity'].setValue('');
+      this.manageOrderForm.controls['total'].setValue('');
+    })
+  }
+
+  getProductDetails(value: any) {
+    this.productService.getById(value.id).subscribe((response:any) => {
+      this.price= response.price;
+      this.manageOrderForm.controls['price'].setValue(response.price);
+      this.manageOrderForm.controls['quantity'].setValue(1);
+      this.manageOrderForm.controls['total'].setValue(this.price * 1);
+    });
+  }  
+
+  setQuantity(value: any) {
+    var temp= this.manageOrderForm.controls['quantity'].value;
+    if (temp > 0) this.manageOrderForm.comtrols['total'].setValue(this.manageOrderForm.controls['quantity'].value * this.manageOrderForm.controls['price'].value);
+    else 
+      if ((temp <= 0) || (temp=""))  {
+        this.manageOrderForm.controls['quantity'].setValue(0);
+        this.manageOrderForm.controls['total'].setValue(0);
+      }
+  }
+
+  validateProductAdd() {
+    if (this.manageOrderForm.controls['total'].value <= 0 || this.manageOrderForm.controls['total'].value === null ||
+        this.manageOrderForm.controls['total'].value <= 0) return true;
+    else
+      return false;    
+  }
+
+  validateSubmit() {
+    //to do
+  }
+
+  add() {
+    var formData= this.manageOrderForm.value;
+    var productName= this.dataSource.find((e: { id: any; }) => e.id === formData.product.id);
+    if (productName === undefined) {
+      this.totalAmount= this.totalAmount + formData.total;
+      this.dataSource.push({id:formData.product.id, name:formData.product.name, category: formData.category.name, quantity: formData.quantity, price: formData.price, total: formData.total});
+      this.dataSource= [...this.dataSource];
+      this.snackbarService.openSnackBar(GlobalConstants.productAdded,"success");
+    }
+  }
+
+  handleDeleteAction(value: any, element: any) {
+    this.totalAmount= this.totalAmount - element.total;
+    this.dataSource.splice(value,1);
+    this.dataSource= [...this.dataSource];
+  }
+
+  submitAction() {
+    
+  }
 }
